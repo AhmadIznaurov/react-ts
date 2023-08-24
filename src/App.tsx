2b665efd1aa8fcab1ebef532a5d69a3c98a5a0d8
AhmadIznaurov <111181823+AhmadIznaurov@users.noreply.github.com>
2023-08-24 17:38:14 +0300

    ts

--- src/App.tsx
+++ src/App.tsx
@@ -1,59 +1,109 @@
-import React, {FC} from 'react';
+import React, {FC, useState} from 'react';
 
-
-
-export const App: FC = () => {
-
-    const handleFocus = (e: React.FocusEvent<HTMLInputElement>) => {
-        console.log(e.currentTarget)
-    }
-
-    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-      e.preventDefault()
-        console.log('submitted')
-    }
-    return (
-        <>
-            <form
-            onSubmit={handleSubmit}
-            >
-                Some easy form text
-                <input
-                    onFocus={handleFocus}
-                    type="text"
-                    name='text'
-                />
-                <br/>
-                <button type='submit'>submit</button>
-            </form>
-        </>
-    )
-
-
+interface Position {
+    id: string,
+    value: string,
+    title: string
 }
 
+type showDataState = {
+    name: string,
+    text: string,
+    position: string
+}
+export const App: FC = () => {
+        const POSITIONS: Array<Position> = [
+            {
+                id: "fd",
+                value: "Frontend Developer",
+                title: "Frontend Developer"
+            },
+            {
+                id: "bd",
+                value: "Backend Developer",
+                title: "Backend Developer"
+            }
+        ]
+
+    const DEFAULT_POSITIONS_VALUE: string = POSITIONS[0].value
+    const styles: React.CSSProperties = { display: 'block', marginBottom: '10px'}
+
+    const [inputText, setInputText] = useState<string>('')
+    const [textAreaText, setTextAreaText] = useState<string>('')
+    const [selectText, setSelectText] = useState<string>(DEFAULT_POSITIONS_VALUE)
+    const [showData, setShowData] = useState<showDataState>( {
+        name: "",
+        text: "",
+        position: ""
+    })
+
+    const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>):void => {
+            const { target: {value: inputText} } = e;
+            setInputText(inputText)
+    }
+
+    const handleAreaChange = (e: React.ChangeEvent<HTMLTextAreaElement>):void => {
+        const { target: {value: textAreaText} } = e;
+        setTextAreaText(textAreaText)
+    }
+
+    const handleSelectChange = (e: React.ChangeEvent<HTMLSelectElement>):void => {
+        const { target: {value: selectText} } = e;
+        setSelectText(selectText)
+    }
+
+    const handleShow = (e: React.MouseEvent<HTMLButtonElement>):void => {
+        e.preventDefault();
+
+        setInputText('')
+        setTextAreaText('')
+        setSelectText(DEFAULT_POSITIONS_VALUE)
+        setShowData({
+            name: inputText,
+            text: textAreaText,
+            position: selectText
+        })
+    }
 
 
-
-
-
-
-
-// export const App: FC = () => {
-//
-//     const [count, setCount] = useState(0);
-//     const handleClick = (e: React.MouseEvent<HTMLButtonElement>) => {
-//         console.log(e.clientX, e.clientY)
-//         setCount(count +1)
-//     }
-//
-//     return (
-//         <>
-//         <h1>Counter</h1>
-//             <h2>{count}</h2>
-//             <button onClick={(e) => handleClick(e)}>plusOne</button>
-//         </>
-//     )
-// }
+    return (
+        <>
+            <form>
+                <label style={styles}>
+                    Name:
+                    <input
+                    value={inputText}
+                    onChange={handleInputChange}
+                    />
+                </label>
+                <label style={styles}>
+                    Text:
+                    <textarea
+                    value={textAreaText}
+                    onChange={handleAreaChange}
+                    />
+                </label>
+                <select
+                style={styles}
+                value={selectText}
+                onChange={handleSelectChange}
+                >
+                    {POSITIONS.map(({id, value, title}) => (
+                        <option
+                        key={id}
+                        value={value}
+                        >
+                            {title}
+                        </option>
+                    ))}
+                </select>
+                <button onClick={handleShow}>Show Data</button>
+            </form>
+            <h2>{showData.name}</h2>
+            <h3>{showData.text}</h3>
+            <h3>{showData.position}</h3>
+        </>
+    )
+}
 
 
